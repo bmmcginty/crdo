62fa82ae762d74cbc7107de95b81bfe6974ae84c
Brandon McGinty (codex) <git@bmcginty.us>
2026-07-21 14:31:21 -0400

    Fold schedule support into store

--- src/crdo/schedule_store.cr
+++ src/crdo/schedule_store.cr
@@ -1,3 +1,21 @@
+record ScheduleReloadPlanEntry,
+  task : Task,
+  current : TaskState?,
+  keep_current : Bool,
+  preserve_state : Bool,
+  retire_current : Bool
+
+record ScheduleReloadPlan,
+  entries : Array(ScheduleReloadPlanEntry),
+  retiring_removed : Array(TaskState),
+  deferred_tasks : Hash(String, Task)
+
+record ScheduleLoadResult,
+  states : Array(TaskState),
+  deferred_tasks : Hash(String, Task),
+  autosave : Time::Span,
+  print_report : Bool
+
 class ScheduleReloadPlanner
   @existing : Array(TaskState)
   @incoming : Array(Task)
